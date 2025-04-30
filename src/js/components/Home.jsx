d57b26c0690c5171b092e2343e6544cc2df40795
Daniela-MV <danmv.0991@gmail.com>
2025-04-30 18:20:08 +0000

    Diseño del ToDo

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,26 +1,63 @@
-import React from "react";
+import React, { useState, useEffect} from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
+
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-            
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+	const [tareas, setTareas] = useState([]);
+	const [nuevaTarea, setNuevaTarea] = useState("");
+	const [borrarBoton, setBorrarBoton] = useState (null);
+    
+	
+	function entrada(evento){
+       setNuevaTarea(evento.target.value);
+	}
+
+	function inputEnter(e){
+		if (e.key === "Enter" && nuevaTarea.trim() !== "") {
+			setTareas([...tareas, nuevaTarea]);
+			setNuevaTarea("");
+		}
+	}
+
+	function borrarTarea(index){
+		const nuevasTareas = tareas.filter((_, i)=> i !== index);
+		setTareas(nuevasTareas);
+	}
+
+	function tareasActivas (){
+		return tareas.length;
+	}
+
+	return (
+	 <div className="fondo d-flex justify-content-center">
+	 	<div className="form text-center font-monospace fs-1 mt-5" style={{width: "700px"}}>
+	 	<label htmlFor="tareaInput" className="estilo" >Tareas</label>
+	 		<input 
+	 		type="tareaInput" 
+	 		className="form-control" 
+	 		value={nuevaTarea}  
+	 		placeholder="Añade una tarea" 
+	 		onChange={entrada} 	
+	 		onKeyDown={inputEnter}/>	
+
+                <ul className="bg-light-subtle" style={{width: "700px"}}>
+				{tareas.map((tarea, index)=> 
+				<li className="d-flex justify-content-between align-items-center px-3 py-2 border-bottom rounded-3" id="homework" key={index} onMouseEnter={()=>setBorrarBoton(index)} onMouseLeave={() =>setBorrarBoton(null)}> 
+				<span className="text">
+				<i className="bi bi-check-circle me-2 fs-4 text-success"></i>{tarea}</span>
+				{borrarBoton === index && (
+				<button type="button" class="btn btn-light" onClick={()=>borrarTarea(index)}>
+				<i className="bi bi-clipboard-x"></i> 
+				</button> )}
+				</li>)}
+				<span className="d-flex justify-content-start fs-5 text-secondary">{tareasActivas()} tareas</span>
+			    </ul>
+			</div>
+		
 		</div>
 	);
 };
